0a8e873e451fc6cd7f103396465b9c2ab53eee0c
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2020-06-21 09:04:01 +0200

    move content types into separate schema

--- lib/real_world_web/schema.ex
+++ lib/real_world_web/schema.ex
@@ -1,38 +1,11 @@
 defmodule RealWorldWeb.Schema do
   use Absinthe.Schema
 
-  object :article do
-    field :id, :id
-    field :slug, :string
-    field :title, :string
-    field :description, :string
-    field :body, :string
-    field :tags, list_of(:string)
-    field :updated_at, :date_time
-    field :viewer_has_favorited, :integer
-    field :favorites_count, :integer
-
-    field :created_at, :date_time do
-      resolve(fn parent, _, _ ->
-        {:ok, Map.get(parent, :inserted_at)}
-      end)
-    end
-  end
-
-  object :article_edge do
-    field :node, :article
-    field :cursor, :string
-  end
+  import_types __MODULE__.Content
 
   object :page_info do
     field :end_cursor, :string
     field :has_next_page, :boolean
-  end
-
-  object :article_connection do
-    field :edges, list_of(:article_edge)
-    field :page_info, :page_info
-    field :total_count, :integer
   end
 
   object :profile do
---
+++ lib/real_world_web/schema/content.ex
@@ -0,0 +1,32 @@
+defmodule RealWorldWeb.Schema.Content do
+  use Absinthe.Schema.Notation
+
+  object :article do
+    field :id, :id
+    field :slug, :string
+    field :title, :string
+    field :description, :string
+    field :body, :string
+    field :tags, list_of(:string)
+    field :updated_at, :date_time
+    field :viewer_has_favorited, :integer
+    field :favorites_count, :integer
+
+    field :created_at, :date_time do
+      resolve(fn parent, _, _ ->
+        {:ok, Map.get(parent, :inserted_at)}
+      end)
+    end
+  end
+
+  object :article_edge do
+    field :node, :article
+    field :cursor, :string
+  end
+
+  object :article_connection do
+    field :edges, list_of(:article_edge)
+    field :page_info, :page_info
+    field :total_count, :integer
+  end
+end
